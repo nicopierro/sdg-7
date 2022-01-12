6cb82646f4483fa3ef4e1535d9140996f91174e3
palo-landrae <58599530+palo-landrae@users.noreply.github.com>
2022-01-12 08:55:35 +0100

    modified main.py (#1)

--- provaGrafici/grafici/main.py
+++ provaGrafici/grafici/main.py
@@ -1,26 +1,26 @@
-from flask import Flask
-from flask import send_file
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+from bson.json_util import dumps, loads
 
 app = Flask(__name__)
-CORS(app)
-
 app.config["MONGO_URI"] = "mongodb+srv://sloth:mSCW5mKaiJOuABCG@cluster0.nljzd.mongodb.net/SDG7?retryWrites=true&w=majority"
+
+CORS(app)
 mongo = PyMongo(app)
 
+@app.route("/")
+# Generic Python functino that returns "Hello world!"
+def index():
+    return "Hello world!"
+
 @app.route("/linee", methods=['GET'])
-# Generic Python function that returns books.json
 def linee():
     ea = mongo.db.ElectricityAccess
-    output = [] 
-    for s in ea.find().limit(100):
-        output.append(s['Entity'])
-    return jsonify({'result': output})
+    list_cur = list(ea.find({},{"_id":0}))
+    return dumps(list_cur)
 
 # Checks to see if the name of the package is the run as the main package.
 if __name__ == "__main__":
     # Runs the Flask application only if the main.py file is being run.
-    app.run(debug=True, port=4000)
+    app.run(port=5000)
