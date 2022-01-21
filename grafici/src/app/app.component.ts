16d0aa55abee9056c649da75a1c84b01d52449b0
ErrichielloEma <errichiello.emanuele@iisgalvanimi.edu.it>
2022-01-21 10:53:15 +0000

    chart data

--- grafici/src/app/app.component.ts
+++ grafici/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ElectricityAccessChartComponent } from './electricity-access-chart/electricity-access-chart.component';
+import { ElectricityAccess } from './models/electricity-access.model';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'grafici';
-}
+  chartDataArray = new Array<ElectricityAccessChartComponent>();
 
+
+}
--- grafici/src/app/electricity-access-chart/electricity-access-chart.component.ts
+++ grafici/src/app/electricity-access-chart/electricity-access-chart.component.ts
@@ -31,25 +31,15 @@ export class ElectricityAccessChartComponent implements OnInit {
   prepareVectData = (data: ElectricityAccess[]) => {
     console.log(data);
     this.chartData = data;
-    this.chartData.push(data);
+
   }
+
 
   ngOnInit(): void {
     this.obsVett = this.http.get<ElectricityAccess[]>(`https://5000-nicopierro-sdg7-gh3wx1dpygt.ws-eu27.gitpod.io/linee`);
     this.obsVett.subscribe(this.prepareVectData);
     
   }
-  
-  this.charts.push({
-    title: 'Area Chart',
-    type: ChartType.AreaChart,
-    columns: ['Year', 'Sales', 'Expenses'],
-    data: [
-      ['2013', 1000, 400],
-      ['2014', 1170, 460],
-      ['2015', 660, 1120],
-      ['2016', 1030, 540]
-    ]
-  });
+
 
 }
