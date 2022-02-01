445dfb448d559f79382b86e412d4c26b388c8469
ErrichielloEma <errichiello.emanuele@iisgalvanimi.edu.it>
2022-02-01 11:45:23 +0000

    Routing beta

--- grafici/src/app/app-routing.module.ts
+++ grafici/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EaDifferenceComponent } from './ea-difference/ea-difference.component';
+import { ElectricityAccessComponent } from './electricity-access/electricity-access.component';
+import { AccessToElectricity2010vs2019 } from './models/ea-difference.model';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ElectricityAccess', component: ElectricityAccessComponent},
+  { path: 'AccessToElectricity2010vs2019', component: EaDifferenceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
